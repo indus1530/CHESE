207d3a74b0e686fd7aebb53d4be6704081985e2c
SAJID LATIF <indus1530@gmail.com>
2021-10-30 17:47:07 +0500

    SectionWSG42Activity added

---
+++ app/src/main/java/edu/aku/hassannaqvi/chese/ui/sections/SectionWSG42Activity.java
@@ -0,0 +1,89 @@
+package edu.aku.hassannaqvi.chese.ui.sections;
+
+import static edu.aku.hassannaqvi.chese.core.MainApp.appInfo;
+import static edu.aku.hassannaqvi.chese.core.MainApp.wsg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import edu.aku.hassannaqvi.chese.MainActivity;
+import edu.aku.hassannaqvi.chese.R;
+import edu.aku.hassannaqvi.chese.contracts.TableContracts.WSGTable;
+import edu.aku.hassannaqvi.chese.database.DatabaseHelper;
+import edu.aku.hassannaqvi.chese.databinding.ActivitySectionWsg42Binding;
+
+
+public class SectionWSG42Activity extends AppCompatActivity {
+    ActivitySectionWsg42Binding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_wsg42);
+        bi.setCallback(this);
+        bi.setWsg(wsg);
+    }
+
+
+    private boolean updateDB() {
+        DatabaseHelper db = appInfo.getDbHelper();
+        int updcount = db.updatesWSGColumn(WSGTable.COLUMN_SWS4, wsg.sWS4toString());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "SORRY! Failed to update DB", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    public void btnContinue(View view) {
+        if (!formValidation()) return;
+        if (!addForm()) return;
+        if (updateDB()) {
+            setResult(2);
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+
+    public void BtnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    private boolean addForm() {
+        if (!wsg.getId().equals("")) return true;
+        DatabaseHelper db = appInfo.dbHelper;
+        long rowid = db.addWSG(wsg);
+        wsg.setId(String.valueOf(rowid));
+        if (rowid > 0) {
+            wsg.setUid(wsg.getDeviceId() + wsg.getId());
+            db.updatesWSGColumn(WSGTable.COLUMN_UID, wsg.getUid());
+            return true;
+        } else {
+            Toast.makeText(this, "Failed to update DB", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
+        setResult(RESULT_CANCELED);
+    }
+}
