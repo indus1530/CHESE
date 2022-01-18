c89e0da2305ddd7e2d70796fa494c5ad9b0ec128
ahs1n <ahsanpervez74@gmail.com>
2022-01-18 10:28:49 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/chese/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/chese/core/MainApp.java
@@ -34,8 +34,8 @@ public class MainApp extends Application {
     public static final String PROJECT_NAME = "CHESE";
     public static final String DIST_ID = null;
     public static final String SYNC_LOGIN = "sync_login";
-    //public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
-    public static final String _IP = "https://cls-pae-fp51764";// .TEST server
+    public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
+    //public static final String _IP = "https://cls-pae-fp51764";// .TEST server
     //public static final String _IP = "http://f38158/smk_hfa";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
     public static final String _HOST_URL = MainApp._IP + "/uen_ph2/api/";// .TEST server;
